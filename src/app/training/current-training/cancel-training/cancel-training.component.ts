1bb4da03d54d218e8cf15c55e80c4355de180cd7
Vladimir Pavković <vladimirpavk@gmail.com>
2018-08-28 13:32:12 +0200

    +

--- src/app/training/current-training/cancel-training/cancel-training.component.ts
+++ src/app/training/current-training/cancel-training/cancel-training.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { ExerciseData } from "../current-training.component";
 
 @Component({
   selector: 'app-cancel-training',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CancelTrainingComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<CancelTrainingComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ExerciseData) {}
 
-  ngOnInit() {
+  ngOnInit() { 
+  }
+
+  actionButtonClicked(action:string){
+    this.dialogRef.close(action);
   }
 
 }
--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -2,6 +2,11 @@ import { Output, Component, OnInit, EventEmitter, Input } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { CancelTrainingComponent } from './cancel-training/cancel-training.component';
 
+export interface ExerciseData{
+  name:string,
+  progress: number
+}
+
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
@@ -10,29 +15,43 @@ import { CancelTrainingComponent } from './cancel-training/cancel-training.compo
 export class CurrentTrainingComponent implements OnInit {
 
   @Input("currentTrainingName") currentTrainingName:string;
-
+  @Output() public nextExercise:EventEmitter<void>=new EventEmitter<void>();
   private currentTrainingProgress:number=0;
   private intervalHandler;
-  @Output() 
-  public nextExercise:EventEmitter<void>=new EventEmitter<void>();
-
+ 
   constructor(private cancelDialog:MatDialog) { }
 
   ngOnInit() {    
+    this.activateExercise(); 
+  }
 
+  private activateExercise(){
     this.intervalHandler=setInterval(()=>{
       this.currentTrainingProgress=this.currentTrainingProgress+10;
       if(this.currentTrainingProgress>=100){
         clearInterval(this.intervalHandler);
       }
     },500);
-
-    
   }
 
   private stopExercise(){
-    //clearInterval(this.intervalHandler);
-    const cancelDialogRef:MatDialogRef<CancelTrainingComponent> = this.cancelDialog.open(CancelTrainingComponent);
+    clearInterval(this.intervalHandler);
+    const cancelDialogRef:MatDialogRef<CancelTrainingComponent> = this.cancelDialog.open(CancelTrainingComponent,
+    {
+      data:{
+        name: this.currentTrainingName,
+        progress: this.currentTrainingProgress
+      }
+    });
+    cancelDialogRef.afterClosed().subscribe((result)=>{
+      if(result=='OK'){
+        this.nextExercise.emit();
+      }
+      else
+      {      
+        this.activateExercise();
+      }
+    });
   }
 
   private goToNext(){
